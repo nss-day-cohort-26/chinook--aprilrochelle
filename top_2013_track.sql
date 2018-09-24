75b036948c1f15f6913d6869bdbfed9485dfbac0
April Nichols <nicholsar@gmail.com>
2018-09-24 09:33:53 -0500

    fixed 2 queries

--- top_2013_track.sql
+++ top_2013_track.sql
@@ -1,10 +1,8 @@
---Provide a query that shows the most purchased track of 2013.
-
-Select t.Name 'Name', Count(il.TrackId) 'Purchases'
+Select t.Name 'Name', Count(t.Name) 'Purchases'
 From InvoiceLine il
 Join Track t on il.TrackId = t.TrackId
 Join Invoice i on il.InvoiceId = i.InvoiceId
 where i.InvoiceDate like '2013%'
-Group by il.TrackId;
-
---No track purchased more than once in 2013
+Group by t.Name
+Order by Purchases desc
+Limit 1;
--- top_5_tracks.sql
+++ top_5_tracks.sql
@@ -1,8 +1,7 @@
---Provide a query that shows the top 5 most purchased songs.
 
 Select t.TrackId 'Track', t.Name 'Name', Count(il.TrackId) 'Purchases'
 from InvoiceLine il
 Join Track t on il.TrackId=t.TrackId
-Group by il.TrackId
+Group by t.Name
 Order by Purchases Desc
 limit 5;
